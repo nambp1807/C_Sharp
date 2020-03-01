4cc2d2095a7ad6f8a3110a73960c2e04185bede8
Admin <admin@g.com>
2020-03-01 17:18:36 +0700

    upload

---
+++ lab2/Lab2.cs
@@ -0,0 +1,14 @@
+﻿namespace t1904a
+{
+    public class Lab2
+    {
+         static void Main(string[] args)
+        {
+             Products pd= new Products();
+             Action a = new Action();
+             
+             a.inputProducts();
+             a.showProduct();
+        }
+    }
+}
