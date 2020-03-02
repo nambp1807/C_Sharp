f84105a84018b4c805d6139d67737ee2232c0f98
Admin <admin@g.com>
2020-03-02 12:36:45 +0700

    upload

--- lab3/Program.cs
+++ lab3/Program.cs
@@ -2,20 +2,6 @@
 {
     public class Program
     {
-         static void Main(string[] args)
-        {
-            Thread t = new Thread(count);
-            t.Start();
-            
-
-        }
-        public static void count()
-        {
-            for(int i = 600;i > 0; i--)
-            {
-                Console.WriteLine(i / 60 +":"+ i % 60);
-                Thread.Sleep(1000);
-            }
-        }
+        
     }
 }
